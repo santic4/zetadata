e0cf5f9195f03371becf5deaae7b47ef0c251143
Santiago Cañu <santisv4@gmail.com>
2025-05-19 02:17:20 -0300

    fix url smoke test server

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -6,7 +6,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
